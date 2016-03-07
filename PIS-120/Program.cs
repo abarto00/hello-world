da82da12160dadb6f0f8985fe27c44167e811004
student <student>
2016-03-07 08:50:27 +0100

    Project creation

---
+++ PIS-120/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIS_120
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
